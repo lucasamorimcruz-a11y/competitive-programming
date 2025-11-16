a549369494c5e77490467117521cbb201dfd3d18
Lucas Amorim <lucas.amorim.cruz@gmail.com>
2025-11-16 16:00:39 -0300

    update

--- cses/bitString.cpp
+++ cses/bitString.cpp
@@ -1,2 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+const long long MOD =  static_cast<long long>(1e9) + 7;
+
+int main(){
+    long long n;
+    cin >> n;
+    long long result = 1;
+    
+    for(int i =0; i < n; ++i){
+    result = (result * 2) % MOD;
+    }
+    cout << result;
+}
