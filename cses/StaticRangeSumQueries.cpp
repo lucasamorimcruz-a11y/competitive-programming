6478bb66638d38e8a49bf6cf9a88b274c8bafb5b
Lucas Amorim <lucas.amorim.cruz@gmail.com>
2025-11-21 08:41:58 -0300

    update

--- cses/StaticRangeSumQueries.cpp
+++ cses/StaticRangeSumQueries.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
+#define int long long 
+int32_t main()
 {
-    int n, q; cin >> n >> q;
-    for(int i = 0; i < )
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        numbers[i] = x;
+    }
+    vector<int> results;
+    vector<int> prefix_sum(n + 1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        prefix_sum[i + 1] = prefix_sum[i] + numbers[i];
+    }
+    for (int i = 0; i < q; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << (prefix_sum[b] - prefix_sum[a - 1]) << '\n';
+    }
+
+    
 }
