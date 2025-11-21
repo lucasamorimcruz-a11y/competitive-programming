5f9dfc3e5e39e215fb1a44783cb2eda901802faa
Lucas Amorim <lucas.amorim.cruz@gmail.com>
2025-11-21 08:01:35 -0300

    update

--- cses/minimizingCoins.cpp
+++ cses/minimizingCoins.cpp
@@ -1,2 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 1e6;
+vector<long long> dp (maxn, 0);
+
+long long solve(int n)
+{
+    if (n <= 1){
+        return dp[n];
+    }
+    for (int i = 0; i < n; i++){
+        dp[i] = 0;
+    } 
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    dp[0] = 1;
+    dp[1] = 1;
+}
---
+++ cses/numberSpiral.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        vector<vector<int>> spiral(maxn, vector<int>(maxn));
+        for (int i = 0; i < maxn; i++)
+        {
+            for (int j = 0; j < maxn; j++)
+            {
+                if (y < x)
+                {
+                    
+                }
+            }
+        }
+        int num = spiral[x][y];
+        cout << num << '\n';
+    }
+}
