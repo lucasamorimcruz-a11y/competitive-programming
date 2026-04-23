caf440c96435a3ef612cc49bbee3992843e31ae7
lucas <lucas.amorim.cruz@gmail.com>
2026-04-23 18:07:23 -0300

    mais problemas

--- obi/pratique/2021/fase2/lista_palindromo.cpp
+++ obi/pratique/2021/fase2/lista_palindromo.cpp
@@ -8,8 +8,24 @@ int32_t main(){
     for (auto &x : a) cin >> x;
     bool ok = false;
     int ops = 0;
-    while (!ok && a.size() != 1){
-        if ()
+    int r = a.size() - 1;
+    int l = 0;
+    while (l < r){
+        if (a[l] == a[r]){
+            l++;
+            r--;
+        }
+        else if (a[r] > a[l]){
+            a[l+1] += a[l];
+            l++;
+            ops++;
+        }
+        else {
+            a[r-1] += a[r];
+            r--;
+            ops++;            
+        }
     }
+    cout << ops << '\n';
     return 0;
 }
