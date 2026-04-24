9014a79cb8a23c68e47c2c93f675faa7a5a21df9
lucas <lucas.amorim.cruz@gmail.com>
2026-04-24 19:08:31 -0300

    mais problemas

--- codeforces/ab_balance.cpp
+++ codeforces/ab_balance.cpp
@@ -6,21 +6,11 @@ int32_t main(){
     int t; cin >> t;
     while (t--){
         string s; cin >> s;
-        string ab = "ab";
-        string ba = "ba";
-        int ab_counter=  0;
-        int ba_counter = 0;
-        for (int i = 0; i < s.size() - 1; i++){
-            if (ab == s.substr(i, 2)){
-                ab_counter++;
-            }
-            else if (ba == s.substr(i,2)){
-                ba_counter++;
-            }
+        if (s[0] == s[s.size() - 1]) cout << s << '\n';
+        else{
+            s[n-1] = s[0];
+            cout << s << '\n';
         }
-        if (ba_counter > ab_counter)[
-            
-        ]
     }
     return 0;
 }
---
+++ codeforces/new_year_permutation_concatenation.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+void dp (int n){
+    if (n == 1) return 1;
+}
+int32_t main(){
+    int n; cin >> n;
+    while (n--){
+        dp(n);    
+    }
+    return 0;
+}
