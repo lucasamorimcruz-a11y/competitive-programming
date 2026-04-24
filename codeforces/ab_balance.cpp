8c1157ee823d450b5bbeeec7ec8dae80316fd0e8
lucas <lucas.amorim.cruz@gmail.com>
2026-04-24 19:08:58 -0300

    mais problemas

--- codeforces/ab_balance.cpp
+++ codeforces/ab_balance.cpp
@@ -8,7 +8,7 @@ int32_t main(){
         string s; cin >> s;
         if (s[0] == s[s.size() - 1]) cout << s << '\n';
         else{
-            s[n-1] = s[0];
+            s[s.size()-1] = s[0];
             cout << s << '\n';
         }
     }
